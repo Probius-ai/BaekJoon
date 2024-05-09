bb6a8f9921ca2eb0e8f5ca145cc0a5cdda153c8f
Jonghyeok Lee <kcfm0529@naver.com>
2024-05-09 20:44:55 +0900

    added club questions 05-09 Thursday

---
+++ Java/Q11659.java
@@ -0,0 +1,33 @@
+package Java;
+
+import java.util.Scanner; 
+
+/**
+ * Q11659
+ */
+public class Q11659 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int Arrays[] = new int[N];
+        for(int i=0;i<N;i++){
+            Arrays[i] = sc.nextInt();
+        }
+
+        int S_list[] = new int[N+1];
+        for(int i =1;i<=N;i++){
+            S_list[i]=S_list[i-1]+Arrays[i-1];
+        }
+        int result;
+        for(int k=0; k<M;k++){
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            
+            result = S_list[j]-S_list[i-1];
+            System.out.println(result);
+        }
+    }
+}
